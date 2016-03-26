0571c8943eb4fbb38f7db07db3ba8fdf666c43e2
Malte Meyer <malte.emquadat@gmail.com>
2016-03-26 12:18:17 +0000

    code cleanup and centering of sorting options

--- app/controllers/nanController.js
+++ app/controllers/nanController.js
@@ -1,18 +1,18 @@
 /*  Copyright (C) 2016  emq
-*
-*    This program is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*    (at your option) any later version.
-*
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-*
-*    You should have received a copy of the GNU General Public License
-*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /* global angular */
 /* global jwplayer playerInstance*/
@@ -64,13 +64,13 @@ nanController.controller("nanController", function($scope, $location, $anchorScr
         $scope.predicate = predicate;
         $scope.btnclTitle = '';
     };
-    
+
     $scope.setSortClass = function(button) {
-        switch(button) {
+        switch (button) {
             case 'title':
                 $scope.btnclTitle = 'active';
                 $scope.btnclRdate = '';
-                switch($scope.reverse) {
+                switch ($scope.reverse) {
                     case true:
                         $scope.btnTitleIco = 'fa fa-sort-desc';
                         $scope.btnRdateIco = '';
@@ -84,9 +84,7 @@ nanController.controller("nanController", function($scope, $location, $anchorScr
             case 'orderByDate':
                 $scope.btnclRdate = 'active';
                 $scope.btnclTitle = '';
-                
-                
-                switch($scope.reverse) {
+                switch ($scope.reverse) {
                     case true:
                         $scope.btnTitleIco = '';
                         $scope.btnRdateIco = 'fa fa-sort-desc';
@@ -110,13 +108,13 @@ nanController.controller("nanController", function($scope, $location, $anchorScr
             return undefined;
         }
     };
-    
+
     $scope.orderByDate = function(item) {
         var parts = item.releasedate.split('-');
-        var date = new Date(parseInt(parts[2]), 
-                            parseInt(parts[1]), 
-                            parseInt(parts[0]));
-    
+        var date = new Date(parseInt(parts[2]),
+            parseInt(parts[1]),
+            parseInt(parts[0]));
+
         return date;
     };
 
@@ -126,8 +124,11 @@ nanController.controller("nanController", function($scope, $location, $anchorScr
                 $scope.nanItems = result;
                 $scope.init = true;
             },
-              function(errorResponse) { console.log('error: ' + errorResponse.status); $scope.error = errorResponse }
-            );
+            function(errorResponse) {
+                console.log('error: ' + errorResponse.status);
+                $scope.error = errorResponse
+            }
+        );
     }
     getItems();
 
